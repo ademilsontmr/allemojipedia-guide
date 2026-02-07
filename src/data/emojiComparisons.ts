aed65ac78d8904aa1fe78038d240020f767d87c0
Ademilson Ramos <contato@engenhariae.com.br>
2026-02-07 09:40:01 -0300

    Add 60+ more popular emoji comparisons with high search volume

---
+++ src/data/emojiComparisons.ts
@@ -0,0 +1,209 @@
+// Popular emoji comparisons - shared between sitemap and pages
+export const popularComparisons = [
+  // Smileys - Very similar faces
+  { slug1: 'grinning-face', slug2: 'grinning-face-with-big-eyes' },
+  { slug1: 'grinning-face-with-smiling-eyes', slug2: 'beaming-face-with-smiling-eyes' },
+  { slug1: 'face-with-tears-of-joy', slug2: 'rolling-on-the-floor-laughing' },
+  { slug1: 'slightly-smiling-face', slug2: 'smiling-face' },
+  { slug1: 'smiling-face-with-smiling-eyes', slug2: 'smiling-face' },
+  { slug1: 'upside-down-face', slug2: 'slightly-smiling-face' },
+  
+  // Love & Hearts
+  { slug1: 'smiling-face-with-hearts', slug2: 'smiling-face-with-heart-eyes' },
+  { slug1: 'face-blowing-a-kiss', slug2: 'kissing-face' },
+  { slug1: 'red-heart', slug2: 'pink-heart' },
+  { slug1: 'red-heart', slug2: 'orange-heart' },
+  { slug1: 'red-heart', slug2: 'yellow-heart' },
+  { slug1: 'red-heart', slug2: 'purple-heart' },
+  { slug1: 'red-heart', slug2: 'blue-heart' },
+  { slug1: 'red-heart', slug2: 'black-heart' },
+  { slug1: 'sparkling-heart', slug2: 'two-hearts' },
+  { slug1: 'heart-with-arrow', slug2: 'heart-with-ribbon' },
+  { slug1: 'beating-heart', slug2: 'growing-heart' },
+  { slug1: 'broken-heart', slug2: 'mending-heart' },
+  
+  // Thinking & Confused
+  { slug1: 'thinking-face', slug2: 'face-with-raised-eyebrow' },
+  { slug1: 'confused-face', slug2: 'worried-face' },
+  { slug1: 'neutral-face', slug2: 'expressionless-face' },
+  { slug1: 'face-with-rolling-eyes', slug2: 'unamused-face' },
+  { slug1: 'smirking-face', slug2: 'face-with-raised-eyebrow' },
+  
+  // Sad & Crying
+  { slug1: 'crying-face', slug2: 'loudly-crying-face' },
+  { slug1: 'pleading-face', slug2: 'crying-face' },
+  { slug1: 'disappointed-face', slug2: 'pensive-face' },
+  { slug1: 'weary-face', slug2: 'tired-face' },
+  
+  // Angry & Frustrated
+  { slug1: 'angry-face', slug2: 'pouting-face' },
+  { slug1: 'face-with-symbols-on-mouth', slug2: 'angry-face' },
+  
+  // Winking & Playful
+  { slug1: 'winking-face', slug2: 'smiling-face-with-smiling-eyes' },
+  { slug1: 'winking-face-with-tongue', slug2: 'face-with-tongue' },
+  { slug1: 'zany-face', slug2: 'squinting-face-with-tongue' },
+  
+  // Surprised & Shocked
+  { slug1: 'face-with-open-mouth', slug2: 'hushed-face' },
+  { slug1: 'astonished-face', slug2: 'face-screaming-in-fear' },
+  { slug1: 'flushed-face', slug2: 'hot-face' },
+  
+  // Sick & Unwell
+  { slug1: 'face-with-thermometer', slug2: 'nauseated-face' },
+  { slug1: 'sneezing-face', slug2: 'face-with-medical-mask' },
+  { slug1: 'dizzy-face', slug2: 'woozy-face' },
+  
+  // Cool & Party
+  { slug1: 'smiling-face-with-sunglasses', slug2: 'nerd-face' },
+  { slug1: 'partying-face', slug2: 'cowboy-hat-face' },
+  
+  // Hands & Gestures
+  { slug1: 'thumbs-up', slug2: 'thumbs-down' },
+  { slug1: 'waving-hand', slug2: 'raised-hand' },
+  { slug1: 'clapping-hands', slug2: 'raising-hands' },
+  { slug1: 'folded-hands', slug2: 'palms-up-together' },
+  { slug1: 'victory-hand', slug2: 'crossed-fingers' },
+  { slug1: 'ok-hand', slug2: 'pinched-fingers' },
+  { slug1: 'raised-fist', slug2: 'oncoming-fist' },
+  { slug1: 'love-you-gesture', slug2: 'sign-of-the-horns' },
+  { slug1: 'writing-hand', slug2: 'selfie' },
+  
+  // Monkeys
+  { slug1: 'see-no-evil-monkey', slug2: 'hear-no-evil-monkey' },
+  { slug1: 'hear-no-evil-monkey', slug2: 'speak-no-evil-monkey' },
+  
+  // Cat faces
+  { slug1: 'grinning-cat', slug2: 'grinning-cat-with-smiling-eyes' },
+  { slug1: 'smiling-cat-with-heart-eyes', slug2: 'kissing-cat' },
+  { slug1: 'cat-with-tears-of-joy', slug2: 'grinning-cat' },
+  { slug1: 'crying-cat', slug2: 'weary-cat' },
+  { slug1: 'pouting-cat', slug2: 'cat-with-wry-smile' },
+  
+  // More hands
+  { slug1: 'call-me-hand', slug2: 'waving-hand' },
+  { slug1: 'flexed-biceps', slug2: 'raised-fist' },
+  { slug1: 'heart-hands', slug2: 'folded-hands' },
+  { slug1: 'pinching-hand', slug2: 'pinched-fingers' },
+  { slug1: 'open-hands', slug2: 'palms-up-together' },
+  
+  // Sleepy
+  { slug1: 'sleepy-face', slug2: 'sleeping-face' },
+  { slug1: 'yawning-face', slug2: 'sleepy-face' },
+  { slug1: 'drooling-face', slug2: 'sleeping-face' },
+  
+  // More special faces
+  { slug1: 'face-with-hand-over-mouth', slug2: 'shushing-face' },
+  { slug1: 'zipper-mouth-face', slug2: 'shushing-face' },
+  { slug1: 'face-without-mouth', slug2: 'neutral-face' },
+  { slug1: 'money-mouth-face', slug2: 'smiling-face' },
+  { slug1: 'hugging-face', slug2: 'smiling-face-with-smiling-eyes' },
+  { slug1: 'star-struck', slug2: 'smiling-face-with-heart-eyes' },
+  { slug1: 'smiling-face-with-halo', slug2: 'smiling-face' },
+  { slug1: 'face-with-monocle', slug2: 'thinking-face' },
+  { slug1: 'nerd-face', slug2: 'face-with-monocle' },
+  { slug1: 'disguised-face', slug2: 'smiling-face-with-sunglasses' },
+  { slug1: 'exploding-head', slug2: 'dizzy-face' },
+  { slug1: 'face-vomiting', slug2: 'nauseated-face' },
+  { slug1: 'anxious-face-with-sweat', slug2: 'fearful-face' },
+  { slug1: 'sad-but-relieved-face', slug2: 'crying-face' },
+  { slug1: 'confounded-face', slug2: 'persevering-face' },
+  { slug1: 'downcast-face-with-sweat', slug2: 'disappointed-face' },
+  { slug1: 'frowning-face', slug2: 'slightly-frowning-face' },
+  { slug1: 'frowning-face-with-open-mouth', slug2: 'anguished-face' },
+  { slug1: 'relieved-face', slug2: 'pensive-face' },
+  { slug1: 'grimacing-face', slug2: 'neutral-face' },
+  { slug1: 'lying-face', slug2: 'smirking-face' },
+  
+  // More hearts
+  { slug1: 'heart-on-fire', slug2: 'red-heart' },
+  { slug1: 'heart-on-fire', slug2: 'sparkling-heart' },
+  { slug1: 'revolving-hearts', slug2: 'two-hearts' },
+  { slug1: 'heart-decoration', slug2: 'sparkling-heart' },
+  { slug1: 'heart-exclamation', slug2: 'red-heart' },
+  { slug1: 'kiss-mark', slug2: 'face-blowing-a-kiss' },
+  { slug1: 'love-letter', slug2: 'kiss-mark' },
+  { slug1: 'white-heart', slug2: 'red-heart' },
+  { slug1: 'brown-heart', slug2: 'red-heart' },
+  { slug1: 'grey-heart', slug2: 'black-heart' },
+  
+  // Temperature & health
+  { slug1: 'hot-face', slug2: 'cold-face' },
+  { slug1: 'hot-face', slug2: 'face-with-thermometer' },
+  { slug1: 'face-with-head-bandage', slug2: 'face-with-thermometer' },
+  
+  // More kissing variations (high search volume)
+  { slug1: 'kissing-face', slug2: 'kissing-face-with-closed-eyes' },
+  { slug1: 'kissing-face-with-smiling-eyes', slug2: 'kissing-face' },
+  { slug1: 'kissing-face-with-closed-eyes', slug2: 'kissing-face-with-smiling-eyes' },
+  { slug1: 'face-blowing-a-kiss', slug2: 'kissing-face-with-smiling-eyes' },
+  
+  // More smiling variations (high search volume)
+  { slug1: 'grinning-face-with-sweat', slug2: 'grinning-face' },
+  { slug1: 'grinning-squinting-face', slug2: 'grinning-face-with-smiling-eyes' },
+  { slug1: 'grinning-squinting-face', slug2: 'beaming-face-with-smiling-eyes' },
+  { slug1: 'smiling-face-with-tear', slug2: 'crying-face' },
+  { slug1: 'smiling-face-with-tear', slug2: 'smiling-face' },
+  
+  // Evil & mischievous
+  { slug1: 'smiling-face-with-horns', slug2: 'angry-face-with-horns' },
+  { slug1: 'smiling-face-with-horns', slug2: 'smirking-face' },
+  { slug1: 'angry-face-with-horns', slug2: 'pouting-face' },
+  
+  // Skull & death
+  { slug1: 'skull', slug2: 'skull-and-crossbones' },
+  { slug1: 'skull', slug2: 'ghost' },
+  { slug1: 'ghost', slug2: 'alien' },
+  
+  // Meme comparisons
+  { slug1: 'pile-of-poo', slug2: 'clown-face' },
+  { slug1: 'pile-of-poo', slug2: 'smiling-face' },
+  { slug1: 'clown-face', slug2: 'face-with-monocle' },
+  
+  // Aliens & robots
+  { slug1: 'alien', slug2: 'alien-monster' },
+  { slug1: 'robot', slug2: 'alien' },
+  { slug1: 'robot', slug2: 'nerd-face' },
+  
+  // Monsters
+  { slug1: 'ogre', slug2: 'goblin' },
+  { slug1: 'ogre', slug2: 'angry-face-with-horns' },
+  { slug1: 'goblin', slug2: 'alien-monster' },
+  
+  // More hand gestures
+  { slug1: 'handshake', slug2: 'clapping-hands' },
+  { slug1: 'handshake', slug2: 'folded-hands' },
+  { slug1: 'middle-finger', slug2: 'thumbs-down' },
+  { slug1: 'middle-finger', slug2: 'angry-face' },
+  { slug1: 'backhand-index-pointing-left', slug2: 'backhand-index-pointing-right' },
+  { slug1: 'backhand-index-pointing-up', slug2: 'backhand-index-pointing-down' },
+  { slug1: 'index-pointing-up', slug2: 'backhand-index-pointing-up' },
+  { slug1: 'vulcan-salute', slug2: 'victory-hand' },
+  { slug1: 'raised-back-of-hand', slug2: 'raised-hand' },
+  
+  // More heart combinations
+  { slug1: 'pink-heart', slug2: 'purple-heart' },
+  { slug1: 'orange-heart', slug2: 'yellow-heart' },
+  { slug1: 'blue-heart', slug2: 'purple-heart' },
+  { slug1: 'green-heart', slug2: 'blue-heart' },
+  { slug1: 'white-heart', slug2: 'black-heart' },
+  { slug1: 'brown-heart', slug2: 'black-heart' },
+  
+  // Tongue out variations
+  { slug1: 'face-with-tongue', slug2: 'squinting-face-with-tongue' },
+  { slug1: 'squinting-face-with-tongue', slug2: 'face-savoring-food' },
+  { slug1: 'face-savoring-food', slug2: 'face-with-tongue' },
+  
+  // Sweat variations
+  { slug1: 'grinning-face-with-sweat', slug2: 'anxious-face-with-sweat' },
+  { slug1: 'downcast-face-with-sweat', slug2: 'anxious-face-with-sweat' },
+  
+  // More special comparisons
+  { slug1: 'face-with-peeking-eye', slug2: 'face-with-hand-over-mouth' },
+  { slug1: 'saluting-face', slug2: 'waving-hand' },
+  { slug1: 'dotted-line-face', slug2: 'face-without-mouth' },
+  { slug1: 'melting-face', slug2: 'hot-face' },
+  { slug1: 'face-holding-back-tears', slug2: 'pleading-face' },
+  { slug1: 'face-in-clouds', slug2: 'dizzy-face' },
+  { slug1: 'cowboy-hat-face', slug2: 'smiling-face-with-sunglasses' },
+];
